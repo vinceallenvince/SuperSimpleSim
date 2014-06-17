01fa3168d19d24a439d6f8eb720ec61fb344480f
Vince Allen <vince@spotify.com>
2014-06-17 13:10:55 -0400

    Updates gh-pages deploy.

--- deploy-gh-pages.sh
+++ deploy-gh-pages.sh
@@ -7,10 +7,10 @@ echo "starting deploy..."
  git config user.name "vinceallenvince"
  git config user.email "vince@vinceallen.com"
  cp -r ../public/* ./
- cp -r ../doc/ ./
- cp -r ../plato ./
- cp releases/SuperSimpleSim.min.js ./scripts/SuperSimpleSim.min.js
- cp releases/SuperSimpleSim.min.css ./css/SuperSimpleSim.min.css
+ cp -r ../doc/ ./doc
+ cp -r ../reports ./reports
+ cp ../release/SuperSimpleSim.min.js ./scripts/SuperSimpleSim.min.js
+ cp ../release/SuperSimpleSim.min.css ./css/SuperSimpleSim.min.css
  git add .
  git commit -m "Deployed to Github Pages"
  git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
